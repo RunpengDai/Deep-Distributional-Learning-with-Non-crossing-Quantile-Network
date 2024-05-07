f9dd4fc3057fce64292ce1e9fe08e2db046e0e14
jianingwang_i <wangjn.sufedc@outlook.com>
2024-05-07 14:26:29 +0800

    changing the dependancy

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 conda create -y --name distRL python=3.6.13
+conda init bash
 conda activate distRL
+wget https://www.atarimania.com/roms/Atari-2600-VCS-ROM-Collection.zip
+unzip Atari-2600-VCS-ROM-Collection.zip
+pip install --upgrade pip setuptools wheel
+
 pip install atari-py
-python -m atari_py.import_roms /usr/src/app/Atari
 pip install  -r requirements.txt --verbose
+python -m atari_py.import_roms ROMS
